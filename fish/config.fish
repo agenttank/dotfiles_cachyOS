4ba58d1324c6e2e099efc27d1e2deb4ad2195bf4
Stefan Pinter <stefan@pinter-net.at>
2024-10-19 19:31:15 +0200

    init

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+fish_vi_key_bindings
+
+alias vim="nvim"
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
